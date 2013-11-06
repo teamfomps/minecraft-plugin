1d078f68b80e4752a7daf868eeff28f7da7201d3
Stephen Van Dahm <code@vandahm.com>
2013-11-05 18:02:53 -0600

    Initial Checkin. I have not even run this once.

---
+++ src/com/teamfomps/minecraft/TeamFomps.java
@@ -0,0 +1,12 @@
+package com.teamfomps.minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TeamFomps extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        return;
+    }
+}
