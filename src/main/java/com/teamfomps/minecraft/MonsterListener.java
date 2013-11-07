b89fbbd4f10597c6501b85c5727c7fa10cdf4ded
Stephen Van Dahm <code@vandahm.com>
2013-11-06 23:47:01 -0600

    Added a listener to prevent certain monster spawns

---
+++ src/main/java/com/teamfomps/minecraft/MonsterListener.java
@@ -0,0 +1,69 @@
+package com.teamfomps.minecraft;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.entity.Monster;
+
+import org.bukkit.util.Vector;
+import org.bukkit.util.BlockVector;
+
+import org.bukkit.Location;
+
+import java.util.logging.Logger;
+
+public class MonsterListener implements Listener
+{
+    protected Logger logger;
+
+    public MonsterListener(Logger logger)
+    {
+        this.logger = logger;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void preventHighAltitudeMonsterSpawns(CreatureSpawnEvent event)
+    {
+        // A y-value of 120 is just about cloud level.
+        if (event.getLocation().getY() < 120) {
+            return;
+        }
+
+        // Ignore creatures that aren't monsters
+        if (! (event.getEntity() instanceof Monster)) {
+
+        }
+
+        // Ignore monsters spawned by a plugin.
+        if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.CUSTOM) {
+            return;
+        }
+
+        // OK, prevent the monster from spawning.
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void preventCreaturesFromSpawningNearMyPyramid(CreatureSpawnEvent event)
+    {
+        double x = event.getLocation().getX();
+        double z = event.getLocation().getZ();
+
+        if (x > -115 || x < -193) {
+            return;
+        }
+
+        if (z < 210 || z > 288) {
+            return;
+        }
+
+        // Ignore creatures spawned by a plugin
+        if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.CUSTOM) {
+            return;
+        }
+
+        event.setCancelled(true);
+    }
+}
--- src/main/java/com/teamfomps/minecraft/TeamFomps.java
+++ src/main/java/com/teamfomps/minecraft/TeamFomps.java
@@ -2,11 +2,13 @@ package com.teamfomps.minecraft;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.teamfomps.minecraft.MonsterListener;
+
 public class TeamFomps extends JavaPlugin
 {
     @Override
     public void onEnable()
     {
-        return;
+        getServer().getPluginManager().registerEvents(new MonsterListener(getLogger()), this);
     }
 }
